c9d1456fb515eeb8c62c1490d179907a473c2e68
felipeclarindo <fglpc2@gmail.com>
2024-06-02 23:31:57 -0300

    Implementando funcoes utilitarias

---
+++ database/tools/utils.py
@@ -0,0 +1,17 @@
+from datetime import date
+from enum import StrEnum
+
+class EstadoUser(StrEnum):
+    LIBERADO = "liberado"
+    BLOQUEADO = "bloqueado"
+
+def pegar_data_atual() -> str:
+    data = date.today()
+    return data.strftime("%Y/%m/%d")
+
+def check_user_state(validacoes):
+    for validacao in validacoes:
+        if not validacao:
+            return EstadoUser.BLOQUEADO
+    else:
+        return EstadoUser.LIBERADO
